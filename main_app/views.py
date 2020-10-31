a91ff113559359e74a9499f8d5ee2db7b179db74
cooperama <coope133@gmail.com>
2020-10-31 15:55:51 -0700

    Adds home and about routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+
+
+# ---------------- STATIC
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
